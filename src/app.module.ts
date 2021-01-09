9eee8655f014df28dcf574a4e8726786e821c43c
CECEBORDIER <33873099+CECEBORDIER@users.noreply.github.com>
2021-01-09 21:09:23 +0100

    "auth added, need to finish the delete, and mongoDB"

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConcertsModule } from './concerts/concerts.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, ConcertsModule],
+  imports: [UsersModule, ConcertsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
